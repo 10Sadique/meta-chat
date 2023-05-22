e6326add21439e595ad92cb1bcd2de94afec8b3b
10Sadique <jafarsjahan@gmail.com>
2023-05-22 11:55:37 +0600

    desktop sidebar added

--- app/components/EmptyState.tsx
+++ app/components/EmptyState.tsx
@@ -1,9 +1,13 @@
-import React from 'react';
+'use client';
 
 const EmptyState = () => {
     return (
         <div className="flex items-center justify-center h-full px-4 py-10 bg-gray-100 sm:px-10 lg:px-8">
-            Empty State
+            <div className="flex flex-col items-center text-center">
+                <h3 className="mt-2 text-2xl font-semibold text-gray-900 ">
+                    Select a chat or start a new conversation
+                </h3>
+            </div>
         </div>
     );
 };
